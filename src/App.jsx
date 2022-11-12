11cecc854456455ff645963b3d81f84820a91888
dolkar <lhamodolker10@gmail.com>
2022-11-11 22:08:40 -0800

    Firt commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>
+    
+
+</p>
+<div>Service
+  <div>Web</div>
+  <div>Mobile</div>
+</div>
+<div>Service
+  <div>Web</div>
+  <div>Mobile</div>
+</div>
+
+    </div>
+  )
+}
+
+export default App
